e933dbad9a13e2b2582f529e08c6b81c16275b23
Viktoria Czaran <vczaran@gmail.com>
2024-01-03 12:37:08 -0500

    Initialize game screen

---
+++ game.py
@@ -0,0 +1,15 @@
+import pygame
+
+pygame.init()
+screen = pygame.display.set_mode((400,300))
+pygame.display.update()
+pygame.display.set_caption('Piethon')
+game_over = False
+
+while not game_over:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            game_over = True
+
+pygame.quit()
+quit()
